19b71743ca50472d195201e784f29c80e33b6698
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-08-13 12:57:36 -0300

    Update autostart.sh

--- Repository/home/config/openbox/autostart.sh
+++ Repository/home/config/openbox/autostart.sh
@@ -2,6 +2,8 @@
 #xrandr -s 1600x900
 #xrandr --output DP-2 --rotate left
 #xrandr --output HDMI-2 --same-as HDMI-1
+# Wallpaper
+feh --bg-fill /usr/share/wallpapers/Spiral/default.jpg
 # Always on
 xset -dpms &
 xset s noblank &
