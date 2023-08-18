2bf80d373b4971f2276bf2df7fbdac6a16c0b5a4
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2023-08-17 22:07:05 -0300

    Update autostart.sh

--- Repository/autostart.sh
+++ Repository/autostart.sh
@@ -1,9 +1,7 @@
 # Screen settings
-#xrandr -s 1440x900 &
-#xrandr --output DP-2 --rotate left &
-#xrandr --output HDMI-2 --same-as HDMI-1 &
+xrandr -s 1440x900 &
 # Wallpaper
-feh --bg-fill /usr/share/wallpapers/default.jpg &
+#feh --bg-fill /usr/share/wallpapers/default.jpg &
 # Always on
 xset -dpms &
 xset s off &
